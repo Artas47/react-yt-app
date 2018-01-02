7c46a212907b5c5bb06c59a42ad6dc3e9925acfa
Artas47 <artasss47@gmail.com>
2018-01-03 00:04:36 +0100

    Search bar and video list

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyDXr2oV9IkoFuj39Hqrr2ekzlK6AQ7Exxs'; 
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term:'smth'}, (videos) => {
+      this.setState({videos});
+    });    
+
+  }
+
+
+  render(){
+    return(
+      <div>
+        <SearchBar /> 
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.root'));
